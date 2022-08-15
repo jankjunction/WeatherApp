98ea000b2b6e77e1cf0979ebc586e7ec588d52a3
Micah Bell <micahjoshuabell@gmail.com>
2022-08-15 19:45:29 -0400

    Adjust Styling

--- src/events.js
+++ src/events.js
@@ -3,9 +3,9 @@ import fetchWeather from './fetch_weather';
 const citySubmit = () => {
   let submit = document.getElementById('city-search-submit');
 
-  submit.addEventListener('click', () => {
-    let cityInput = document.getElementById('city-search');
-    let cityWeather = fetchWeather(cityInput.value);
+  submit.addEventListener('click', async () => {
+    let citySearch = document.getElementById('city-search');
+    let cityWeather = await fetchWeather(citySearch.value);
     console.log(cityWeather);
     return cityWeather;
   });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles.css';
 import { buildContainer, buildHeader } from './components';
 import { citySubmit } from './events';
 
