4acebf50e8ca4bef5a67d1e30e95280f09b84748
Micah Bell <micahjoshuabell@gmail.com>
2022-08-15 16:53:26 -0400

    Add getDay functionality to convert_unix_time

--- src/convert_unix_time.js
+++ src/convert_unix_time.js
@@ -14,13 +14,23 @@ const convertUnixTime = (dt) => {
     'Nov',
     'Dec',
   ];
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
   const year = moment.getFullYear();
   const month = months[moment.getMonth()];
+  const day = days[moment.getDay()];
   const date = moment.getDate();
   const hour = moment.getHours();
   const min = moment.getMinutes();
   const sec = moment.getSeconds();
-  const dateObj = { year, month, date, hour, min, sec };
+  const dateObj = { year, month, date, day, hour, min, sec };
   return dateObj;
 };
 
